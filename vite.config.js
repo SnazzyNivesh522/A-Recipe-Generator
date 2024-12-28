5fbff163f8709b717de0e2acafae19dd97feafce
SnazzyNivesh522 <niveshpritmani@gmail.com>
2024-12-28 12:17:53 +0530

    Add homepage and deploy scripts for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  "base": "/AI-Recipe-Generator",
+  "base": "/A-Recipe-Generator",
   plugins: [react()],
 })
