568e4ab86187f3aff2ff165cb3fc701424c79375
SnazzyNivesh522 <niveshpritmani@gmail.com>
2024-12-28 12:32:50 +0530

    updated for api keys

--- src/ai.js
+++ src/ai.js
@@ -9,6 +9,8 @@ const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
 `;
 
+console.log(process.env.VITE_ANTHROPIC_API_KEY);
+console.log(process.env.VITE_HF_ACCESS_TOKEN);
 const anthropic = new Anthropic({
   apiKey: process.env.VITE_ANTHROPIC_API_KEY,
   dangerouslyAllowBrowser: true,
